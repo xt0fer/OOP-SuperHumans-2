f15087e8451dd500cc85fc6bff84331b2615b8fb
xt0fer <kryounger@gmail.com>
2018-02-21 18:00:25 -0500

    changes to original

---
+++ src/main/java/Superpowers/Human.java
@@ -0,0 +1,10 @@
+package Superpowers;
+
+/**
+ * Complete the 'Human' class in the Superpowers Package that has fields for: name, age, gender,
+ * occupation, and address. Also create methods for retreiving and outputing this data to screen.
+ * Then create a SuperHuman class and UNIT TEST that subclasses the first with fields for good or bad,
+ * hero name, super ability. As before, create methods for retrieving field data and printing to screen.
+ */
+public class Human {
+}
---
+++ src/test/java/Superpowers/HumanTest.java
@@ -0,0 +1,5 @@
+package Superpowers;
+
+
+public class HumanTest {
+}
