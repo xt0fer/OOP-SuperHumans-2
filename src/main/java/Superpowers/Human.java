cea45fdf378d3d33e88dbf7632126a06ae350350
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-22 13:51:21 -0500

    just some getters ans setters in human class

--- src/main/java/Superpowers/Human.java
+++ src/main/java/Superpowers/Human.java
@@ -7,4 +7,64 @@ package Superpowers;
  * hero name, super ability. As before, create methods for retrieving field data and printing to screen.
  */
 public class Human {
+
+    //name, age, gender, phonenumber, email, occupation, and address
+
+    String name = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    int age = 0;
+    int phoneNumber = 0;
+    String email = "";
+    String occupation = "";
+    String address = "";
+
+
 }
--- src/test/java/Superpowers/HumanTest.java
+++ src/test/java/Superpowers/HumanTest.java
@@ -1,5 +1,55 @@
 package Superpowers;
 
 
+import org.junit.Test;
+
 public class HumanTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getAge() {
+    }
+
+    @Test
+    public void setAge() {
+    }
+
+    @Test
+    public void getPhoneNumber() {
+    }
+
+    @Test
+    public void setPhoneNumber() {
+    }
+
+    @Test
+    public void getEmail() {
+    }
+
+    @Test
+    public void setEmail() {
+    }
+
+    @Test
+    public void getOccupation() {
+    }
+
+    @Test
+    public void setOccupation() {
+    }
+
+    @Test
+    public void getAddress() {
+    }
+
+    @Test
+    public void setAddress() {
+    }
 }
